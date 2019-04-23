c60ec0d1fb3d6cefeec79f0ae4ed5310c2add7ac
miaoshan <minnieshan@gmail.com>
2019-04-23 15:26:14 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 class Application < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
   # Write your code here!
+  post '/greet' do
+     erb :greet
+  end
 
 end
+
+#in controllers:
+# get 'books/:id' do    #:id --> dynamic varaibale
+     #some code
+   #end
